5da7bf25a8cd5a785d6e3441f4ebd2fd5077a300
Narciso Castro <narcisocastro0802@gmail.com>
2024-06-05 01:19:31 -0700

    Added operator and radius

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,21 +8,24 @@
             int a = 17;
             int b = 4;
 
-            int sum = a + b;
-            int difference = a - b;
-            int product = a * b;
-
             int quotient = a / b;
             int remainder = a % b;
 
             
-            Console.WriteLine($"{a} + {b} is {sum}");
-            Console.WriteLine($"{a} - {b} is {difference}");
-            Console.WriteLine($"{a} * {b} is {product}");
             Console.WriteLine($"{a} / {b} is {quotient}  remainder {remainder}");
-              
 
+            Console.WriteLine("What is the radius of your circle?");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
         }
-       
+        public static double AreaOfCircle(double radius)
+        {
+           var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
     }
 }
