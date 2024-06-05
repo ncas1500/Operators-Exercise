117deec70ba882eaff8b9a223b3a6281a7b69e6c
Narciso Castro <narcisocastro0802@gmail.com>
2024-06-04 22:57:44 -0700

    Added the required Operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Created two integer variables called a and b.
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} - {b} is {difference}");
+            Console.WriteLine($"{a} * {b} is {product}");
+            Console.WriteLine($"{a} / {b} is {quotient}  remainder {remainder}");
+              
+
         }
+       
     }
 }
